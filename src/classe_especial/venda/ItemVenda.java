77ca30d7975f3dbfb592c517865c1b4042d789ea
Reginaldo Batista <reginaldovlog@gmail.com>
2024-12-05 20:05:45 -0300

    adicionada pasta para dividir classe de vendas

---
+++ src/classe_especial/venda/ItemVenda.java
@@ -0,0 +1,16 @@
+package src.classe_especial.venda;
+
+import src.classe_especial.AgregadorGenerico.Item;
+
+public class ItemVenda extends Item {
+
+  public ItemVenda(String nome, double preco, int quantidade) {
+    super(nome, preco, quantidade);
+  }
+
+  @Override
+  public String toString() {
+    return this.getNome() + " - R$ " + this.getPreco() + " - " + this.getQuantidade() + " unidade(s)";
+  }
+
+}
---
+++ src/classe_especial/venda/Venda.java
@@ -0,0 +1,7 @@
+package src.classe_especial.venda;
+
+import src.classe_especial.AgregadorGenerico.Agregador;
+
+public class Venda extends Agregador<ItemVenda> {
+
+}
